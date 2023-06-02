56e1d4d6febb60afcc6d73498422dde9c92e4866
GuillermoA231 <andradajoaquin302@gmail.com>
2023-06-01 23:08:45 -0300

    ejercicio 3 terminado

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Saludo from "./components/Saludo";
+
+function App() {
+  return (
+    <>
+      <Saludo nuevoMensaje="my friend"></Saludo>
+    </>
+  );
+}
+
+export default App;
